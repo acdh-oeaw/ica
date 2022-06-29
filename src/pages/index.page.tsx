8bc1873fe36a7666bfae22f182567ba794d55b95
marcs19 <schmitzmarc@hotmail.com>
2022-06-29 17:20:49 +0200

    add relation lines

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,6 +1,7 @@
 import { PageMetadata } from '@stefanprobst/next-page-metadata'
-import React, { Fragment ,useMemo,useState} from 'react'
-import {Marker,Popup} from 'react-map-gl';
+import React, { Fragment , useCallback, useState} from 'react'
+import {Layer,Popup,Source} from 'react-map-gl';
+import { start } from 'repl';
 
 import { useI18n } from '@/app/i18n/use-i18n'
 import { withDictionaries } from '@/app/i18n/with-dictionaries'
@@ -15,48 +16,124 @@ export default function HomePage(): JSX.Element {
   const titleTemplate = usePageTitleTemplate()
 
   const metadata = { title: t(['common', 'home', 'metadata', 'title']) }
+  const [PopupInfo, setPopupInfo] = useState(null);
 
-  const [popupInfo, setPopupInfo] = useState(null);
-  const json = [
-      {'Name': 'John Gunther', 'longitude': 16.3531, 'latitude': 48.2399}
-  ];
-  const pins = useMemo(
-    () =>
-      {return json.map((place, index) => {return (
-        <Marker
-          key={`marker-${index}`}
-          longitude={place.longitude}
-          latitude={place.latitude}
-          anchor="bottom"
-          onClick={e => {
-            // If we let the click event propagates to the map, it will immediately close the popup
-            // with `closeOnClick: true`
-            e.originalEvent.stopPropagation();
-            setPopupInfo(place);
-          }}
-        >
-        </Marker>
-      )})},
-    []
-  );
+  const geojson = {
+    type: 'FeatureCollection',
+    features: [
+      {type: 'Feature', geometry: {type: 'Point', coordinates: [16.35323,  48.240234]}, properties: {'Name': 'John Gunther', 'Type': 'Person'}},
+      {type: 'Feature', geometry: {type: 'Point', coordinates: [16.36735,  48.213089]}, properties: {'Name': 'Café Louvre', 'Type': 'Place'}},
+      {type: 'Feature', geometry: {type: 'Point', coordinates: [16.361413,  48.197215]}, properties: {'Name': 'Dorothy Thompson', 'Type': 'Person'}},
+    ]
+  };
+
+  const relationGeojson = {
+    type: 'FeatureCollection',
+    features: [],
+  };
+
+  const relationships = {
+    'Visited regularly': {'Café Louvre': ['Dorothy Thompson', 'John Gunther']},
+    'Friends with': {'John Gunther': ['Dorothy Thompson']}
+  };
+  Object.keys(relationships).forEach((relationship) => {
+    Object.keys(relationships[relationship]).forEach((i) => {
+      let startPoint;
+      geojson.features.forEach((point) => {
+        if (point.properties.Name === i) { startPoint = point.geometry.coordinates; }
+      })
+      relationships[relationship][i].forEach((j) => {
+        let endPoint;
+        geojson.features.forEach((point) => {
+          if (point.properties.Name === j) { endPoint = point.geometry.coordinates; }
+        })
+        relationGeojson.features.push(
+          {
+            type: 'Feature', 
+            geometry: {type: 'LineString', coordinates: [startPoint, endPoint]}, 
+            properties: {'Name': relationship}
+          }
+        );
+      })
+    })
+  });
+  
+
+  const layerStyle: Layer = {
+    id: 'data',
+    type: 'circle',
+    paint: {
+      'circle-radius': 10,
+      'circle-color': [
+        "match",
+        ["get", "Type"],
+        "Person", "red",
+        "Place", "green",
+        "yellow"
+      ]
+    }
+  };
+  const lineStyle: Layer = {
+    id: 'relations',
+    type: 'line',
+    layout: {
+      'line-cap': 'round',
+      'line-join': 'round'
+    },
+    paint: {
+      'line-color': [
+        "match",
+        ["get", "Name"],
+        "Visited regularly", "orange",
+        "Friends with", "pink",
+        "yellow"
+      ],
+      'line-width': 5
+    }
+  };
+
+  const onPopup = useCallback(event => {
+    console.log(event);
+    event.originalEvent.stopPropagation();
+    const {
+      features,
+      point: {x, y}
+    } = event;
+    if (features.length > 0) {
+      const PopupedFeature = features && features[0];
+      const lat = event.lngLat.lat;
+      const lng = event.lngLat.lng;
+      const name = features[0].properties.Name;
+      // prettier-ignore
+      setPopupInfo(PopupedFeature && {feature: PopupedFeature, name, lat, lng});
+    } else { setPopupInfo(null); }
+  }, []);
+
   return (
     <Fragment>
       <PageMetadata title={metadata.title} titleTemplate={titleTemplate} />
       <main>
         <h1>Hello, world!</h1>
-        <GeoMap {...baseMap}>
-          {pins}
-          {popupInfo && (
-          <Popup
-            anchor="top"
-            longitude={Number(popupInfo.longitude)}
-            latitude={Number(popupInfo.latitude)}
-            onClose={() => {return setPopupInfo(null)}}
-          >
-            <div>
-              <span>{popupInfo.Name}</span>
-            </div>
-          </Popup>
+        <GeoMap {...baseMap} 
+          interactiveLayerIds={[layerStyle.id, lineStyle.id]}
+          onClick={onPopup} >
+          <Source id="relations" type="geojson" data={relationGeojson}>
+            <Layer {...lineStyle} />
+          </Source>
+          <Source id="data" type="geojson" data={geojson}>
+            <Layer {...layerStyle} />
+          </Source>
+          {PopupInfo && (
+            <Popup
+             anchor="top"
+             longitude={Number(PopupInfo.lng)}
+             latitude={Number(PopupInfo.lat)}
+             onClose={() => {return setPopupInfo(null)}}
+            >
+             <div>
+               <span>{PopupInfo.name}</span>
+             </div>
+           </Popup>
           )}
         </GeoMap>
       </main>
