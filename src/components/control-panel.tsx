a1065a6dc1d923fce2b58d9c5982e3849910b9d6
Marc <77049819+marcs19@users.noreply.github.com>
2022-07-11 12:02:30 +0200

    feat: add pin layer to map (#5)
    * feat: add map component with configurable base layer
    
    * test: mock createObjectUrl which is missing in jsdom
    
    * add marker with popup
    
    * add relation lines
    
    * added layer toggle
    
    * added layer toggle
    
    * feat: add sublayer toggle
    
    * chore: format
    
    * chore: silence type errors
    
    Co-authored-by: Stefan Probst <stefanprobst@posteo.de>

---
+++ src/components/control-panel.tsx
@@ -0,0 +1,85 @@
+import { useState } from 'react'
+
+import styles from '@/styles/control-panel.module.css'
+
+const categories = ['places', 'relations']
+const subCategories = {
+  places: ['person', 'institution'],
+  relations: [
+    'Visited regularly',
+    'Friends with',
+    'Child of',
+    'Doctor of',
+    'Meeting with',
+    'Helped to emigrate',
+    'Studied at',
+  ],
+}
+const basemaps = ['positron', 'voyager', 'dark-matter']
+
+interface ControlPanelProps {
+  onToggleLayer: (name: string, checked: boolean) => void
+  onToggleSubLayer: (subCategory: string, checked: boolean, name: string) => void
+  onToggleBasemap: (baseMap: string, value: string) => void
+}
+
+export function ControlPanel(props: ControlPanelProps): JSX.Element {
+  const [checked, setChecked] = useState(true)
+  const [selected, setSelected] = useState('positron')
+
+  return (
+    <div className={styles['panel']}>
+      <h3>Layers: </h3>
+      {categories.map((name) => {
+        return (
+          <div key={name} className="input">
+            <input
+              type="checkbox"
+              defaultChecked={checked}
+              onChange={(e) => {
+                setChecked(e.target.checked)
+                return props.onToggleLayer(name, e.target.checked)
+              }}
+            />
+            <label>
+              <b> {name}</b>
+            </label>
+            {/* @ts-expect-error TODO: fix me later */}
+            {subCategories[name].map((subCat) => {
+              return (
+                <div key={subCat} className="input" style={{ marginLeft: '10px' }}>
+                  <input
+                    type="checkbox"
+                    defaultChecked={checked}
+                    onChange={(e) => {
+                      setChecked(e.target.checked)
+                      props.onToggleSubLayer(subCat, e.target.checked, name)
+                    }}
+                  />
+                  <label> {subCat}</label>
+                </div>
+              )
+            })}
+          </div>
+        )
+      })}
+      <h3>Basemaps: </h3>
+      {basemaps.map((basemap) => {
+        return (
+          <div key={basemap} className="input">
+            <input
+              type="radio"
+              checked={selected === basemap}
+              value={basemap}
+              onChange={(e) => {
+                setSelected(e.target.value)
+                return props.onToggleBasemap(basemap, e.target.value)
+              }}
+            />
+            <label> {basemap}</label>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,24 +1,238 @@
 import { PageMetadata } from '@stefanprobst/next-page-metadata'
-import { Fragment } from 'react'
+import type { Feature, FeatureCollection } from 'geojson'
+import { Fragment, useCallback, useRef, useState } from 'react'
+import type { MapRef } from 'react-map-gl'
+import { Layer, Popup, Source } from 'react-map-gl'
 
 import { useI18n } from '@/app/i18n/use-i18n'
 import { withDictionaries } from '@/app/i18n/with-dictionaries'
 import { usePageTitleTemplate } from '@/app/metadata/use-page-title-template'
+import { ControlPanel } from '@/components/control-panel'
+import { GeoMap } from '@/components/geo-map'
+import { base as baseMap } from '@/components/geo-map-base-layer.config'
+import { layerStyle, lineStyle } from '@/components/layers'
+import { geojson, relationships } from '@/components/mock-data'
 import { usePersonsPlaces } from '@/lib/use-persons-places'
 
 export const getStaticProps = withDictionaries(['common'])
+
+type PopupContent = {
+  feature: Feature
+  names: Record<string, any>
+  relations: Record<string, any>
+  lat: number
+  lng: number
+}
 
 export default function HomePage(): JSX.Element {
   const { t } = useI18n<'common'>()
   const titleTemplate = usePageTitleTemplate()
 
   const metadata = { title: t(['common', 'home', 'metadata', 'title']) }
+  const [popupInfo, setPopupInfo] = useState<PopupContent | null>(null)
+
+  const relationGeojson: FeatureCollection = {
+    type: 'FeatureCollection',
+    features: [],
+  }
+
+  Object.keys(relationships).forEach((relationship) => {
+    // @ts-expect-error TODO: fix later please
+    Object.keys(relationships[relationship]).forEach((i) => {
+      let startPoint: any
+      let to: any
+      geojson.features.forEach((point) => {
+        // @ts-expect-error TODO: fix later please
+        if (point.properties['name'] === i) {
+          // @ts-expect-error TODO: fix later please
+          startPoint = point.geometry.coordinates
+          // @ts-expect-error TODO: fix later please
+          to = point.properties.name
+        }
+      })
+      // @ts-expect-error TODO: fix later please
+      relationships[relationship][i].forEach((j) => {
+        let endPoint
+        let from
+        geojson.features.forEach((point) => {
+          // @ts-expect-error TODO: fix later please
+          if (point.properties.name === j) {
+            // @ts-expect-error TODO: fix later please
+            endPoint = point.geometry.coordinates
+            // @ts-expect-error TODO: fix later please
+            from = point.properties.name
+          }
+        })
+        relationGeojson.features.push({
+          type: 'Feature',
+          geometry: { type: 'LineString', coordinates: [startPoint, endPoint] },
+          properties: {
+            type: relationship,
+            from: from,
+            to: to,
+            fromCoords: endPoint,
+            toCoords: startPoint,
+          },
+        })
+      })
+    })
+  })
+
+  const onPopup = useCallback((event: any) => {
+    event.originalEvent.stopPropagation()
+    const { features } = event
+    if (features.length > 0) {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      const PopupedFeature = features[0]!
+      const lat = event.lngLat.lat
+      const lng = event.lngLat.lng
+      const names = {}
+      const relations = {}
+      features.forEach((feature: any) => {
+        if (feature.layer.type === 'circle') {
+          if (!Object.keys(names).includes(feature.properties.name)) {
+            // @ts-expect-error TODO: fix later please
+            names[feature.properties.Name] = [feature.properties.type, feature.properties.id]
+          }
+        } else if (feature.layer.type === 'line') {
+          if (!Object.keys(relations).includes(feature.properties.name)) {
+            // @ts-expect-error TODO: fix later please
+            relations[feature.properties.type] = [[feature.properties.to, feature.properties.from]]
+          } else {
+            // @ts-expect-error TODO: fix later please
+            relations[feature.properties.type].push([
+              feature.properties.to,
+              feature.properties.from,
+            ])
+          }
+        }
+      })
+      setPopupInfo({ feature: PopupedFeature, names, relations, lat, lng })
+      if (features.length === 1 && features[0].layer.type === 'line') {
+        mapRef.current?.fitBounds(
+          [features[0].geometry.coordinates[0], features[0].geometry.coordinates[1]],
+          { padding: 100, duration: 2000 },
+        )
+      } else {
+        mapRef.current?.flyTo({ center: [lng, lat], duration: 2000 })
+      }
+    } else {
+      setPopupInfo(null)
+    }
+  }, [])
+
+  const mapRef = useRef<MapRef>(null)
+
+  const onToggleLayer = useCallback((name: string, checked: boolean) => {
+    if (checked === true) {
+      mapRef.current?.getMap().setLayoutProperty(name, 'visibility', 'visible')
+    } else {
+      mapRef.current?.getMap().setLayoutProperty(name, 'visibility', 'none')
+    }
+  }, [])
+
+  const onToggleSubLayer = useCallback((name: string, checked: boolean, category: string) => {
+    // @ts-expect-error TODO: fix later please
+    let filter = mapRef.current?.getMap().getLayer(category).filter.slice(2)
+    if (checked === false) {
+      filter = filter.filter((e: string) => {
+        return e !== name
+      })
+    } else {
+      filter.push(name)
+    }
+    mapRef.current?.getMap().setFilter(category, buildFilter(filter))
+  }, [])
+
+  function buildFilter(arr: Array<string>) {
+    const filter = ['in', 'type']
+    if (arr.length === 0) {
+      return filter
+    }
+    for (let i = 0; i < arr.length; i += 1) {
+      const value = arr[i]
+      if (value != null) {
+        filter.push(value)
+      }
+    }
+    return filter
+  }
+
+  const onToggleBasemap = useCallback((basemap: string) => {
+    mapRef.current
+      ?.getMap()
+      .setStyle(`https://basemaps.cartocdn.com/gl/${basemap}-gl-style/style.json`)
+  }, [])
 
   return (
     <Fragment>
       <PageMetadata title={metadata.title} titleTemplate={titleTemplate} />
       <main>
         <Hero />
+        <GeoMap
+          {...baseMap}
+          interactiveLayerIds={[layerStyle.id!, lineStyle.id!]}
+          onClick={onPopup}
+          ref={mapRef}
+        >
+          <Source id="relations" type="geojson" data={relationGeojson}>
+            <Layer {...lineStyle} />
+          </Source>
+          <Source id="data" type="geojson" data={geojson}>
+            <Layer {...layerStyle} />
+          </Source>
+          {popupInfo && (
+            <Popup
+              anchor="top"
+              longitude={Number(popupInfo.lng)}
+              latitude={Number(popupInfo.lat)}
+              onClose={() => {
+                setPopupInfo(null)
+              }}
+            >
+              <div>
+                {Object.keys(popupInfo.names).length > 0 && <b>Located here:</b>}
+                {Object.keys(popupInfo.names).map((name) => {
+                  return (
+                    <div key={name}>
+                      <a
+                        href={`https://ica.acdh-dev.oeaw.ac.at/apis/entities/entity/${popupInfo.names[name][0]}/${popupInfo.names[name][1]}/detail`}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        <u>{name}</u>
+                      </a>
+                    </div>
+                  )
+                })}
+                {Object.keys(popupInfo.relations).length > 0 && <b>Relations:</b>}
+                {Object.keys(popupInfo.relations).map((relation) => {
+                  return (
+                    <div key={relation}>
+                      <span>
+                        <em>{relation}:</em>
+                      </span>
+                      {popupInfo.relations[relation].map((r: any) => {
+                        return (
+                          <div key={r}>
+                            <span>
+                              {r[0]} -&gt; {r[1]}
+                            </span>
+                          </div>
+                        )
+                      })}
+                    </div>
+                  )
+                })}
+              </div>
+            </Popup>
+          )}
+          <ControlPanel
+            onToggleLayer={onToggleLayer}
+            onToggleBasemap={onToggleBasemap}
+            onToggleSubLayer={onToggleSubLayer}
+          />
+        </GeoMap>
         <PlacesSection />
       </main>
     </Fragment>
