ff364f4a67ad1da5ca9bd976bf86f5e02139c574
Stefan Probst <stefanprobst@posteo.de>
2022-09-16 08:16:39 +0200

    chore: add clearer error message when redmine issue id is missing

--- config/imprint.config.ts
+++ config/imprint.config.ts
@@ -1,3 +1,4 @@
+import { assert } from '@stefanprobst/assert'
 import { createUrl } from '@stefanprobst/request'
 
 import type { Locale } from '~/config/i18n.config'
@@ -8,6 +9,11 @@ const redmineId = process.env['NEXT_PUBLIC_REDMINE_ID']
  * @see https://fundament.acdh.oeaw.ac.at/imprint-service
  */
 export function createImprintUrl(locale: Locale): URL {
+  assert(
+    redmineId != null,
+    'Failed to generate imprint. Please provide a redmine service issue via NEXT_PUBLIC_REDMINE_ID environment variable.',
+  )
+
   return createUrl({
     baseUrl: 'https://shared.acdh.oeaw.ac.at',
     pathname: '/acdh-common-assets/api/imprint.php',
