7c5cbc91166d50465a47501e1065f410dba3ee2b
Stefan Probst <stefanprobst@posteo.de>
2023-08-31 19:07:25 +0200

    refactor: use new imprint service [skip ci]

--- config/imprint.config.ts
+++ config/imprint.config.ts
@@ -6,7 +6,7 @@ import { type Locale } from "~/config/i18n.config";
 const redmineId = process.env["NEXT_PUBLIC_REDMINE_ID"];
 
 /**
- * @see https://fundament.acdh.oeaw.ac.at/imprint-service
+ * @see https://github.com/acdh-oeaw/imprint-service
  */
 export function createImprintUrl(locale: Locale): URL {
 	assert(
@@ -15,11 +15,8 @@ export function createImprintUrl(locale: Locale): URL {
 	);
 
 	return createUrl({
-		baseUrl: "https://shared.acdh.oeaw.ac.at",
-		pathname: "/acdh-common-assets/api/imprint.php",
-		searchParams: {
-			serviceID: redmineId,
-			outputLang: locale,
-		},
+		baseUrl: "https://imprint.acdh.oeaw.ac.at",
+		pathname: `/${redmineId}`,
+		searchParams: { locale },
 	});
 }
