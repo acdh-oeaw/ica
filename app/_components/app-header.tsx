369129c4d322b198bffe11db84cb234cb20e7aa4
Stefan Probst <stefanprobst@posteo.de>
2025-02-04 20:30:13 +0100

    chore: remove in-progress label

--- app/_components/app-header.tsx
+++ app/_components/app-header.tsx
@@ -32,7 +32,7 @@ export function AppHeader(): ReactNode {
 					>
 						{links.home.label}
 					</Link>
-					<strong className="ml-4">(work in progress)</strong>
+					{/* <strong className="ml-4">(work in progress)</strong> */}
 				</div>
 				<nav>
 					<ul className="flex flex-wrap items-center justify-end gap-x-4 gap-y-2" role="list">
