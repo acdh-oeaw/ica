5fbfa5c7cf48a572621d5a42180dd08dae4f0376
Stefan Probst <stefanprobst@posteo.de>
2025-02-05 11:24:22 +0100

    perf: add extra image device-size for smaller viewports

--- next.config.ts
+++ next.config.ts
@@ -29,6 +29,9 @@ const config: NextConfig = {
 
 		return Promise.resolve(headers);
 	},
+	images: {
+		deviceSizes: [480, 640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+	},
 	logging: {
 		fetches: {
 			fullUrl: true,
